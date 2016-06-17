e856219910de0a4b28f7574ac040bec0e885adf9
Andrii Vasyliev <sol@hiqdev.com>
2016-06-17 11:15:56 +0000

    splitted Builder and Parser

---
+++ src/history/Parser.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace hiqdev\chkipper\history;
+
+class Parser extends Builder
+{
+    public function parsePath($path, $minimal = null)
+    {
+        $this->addTag($this->getLastTag());
+
+        $lines = is_file($path) ? file($path) : [];
+        $no = 0;
+        foreach ($lines as $str) {
+            $str = rtrim($str);
+            ++$no;
+
+            /// skip empty lines
+            if (!$str) {
+
+            /// headers
+            } elseif ($no <= 2 || preg_match('/^# /', $str)) {
+                $this->addHeader($str);
+
+            /// links
+            } elseif (preg_match('/^\[(.*?)\]:\s+(.*)$/', $str, $m)) {
+                $this->addLink($m[1], $m[2]);
+
+            /// tags
+            } elseif (preg_match('/^##\s+\[(.+)\]\s*-?\s*(.*)$/', $str, $m)) {
+                $this->addTag($m[1], $m[2]);
+
+            /// notes
+            } elseif (preg_match('/^-\s+(.*)$/', $str, $m)) {
+                $this->addNote($m[1]);
+
+            /// commits
+            } elseif (preg_match('/^\s+-\s+\[?([0-9a-fA-F]{7})\]?\s*(.*)$/', $str, $m)) {
+                $this->addCommit($m[1], $m[2]);
+
+            /// comments
+            } else {
+                $this->addComment($str);
+            }
+        }
+    }
+
+}
