ebe319bb4c195650791b839c8f545c4f296f809e
Andrii Vasyliev <sol@hiqdev.com>
2016-06-17 17:57:44 +0000

    removed old from History

--- src/history/History.php
+++ src/history/History.php
@@ -145,60 +145,6 @@ class History
         }
     }
 
-    public function render($data)
-    {
-        $res = static::renderHeader('commits history');
-
-        foreach ($this->_tags as $tag => $notes) {
-            $prev = $res;
-            $tag  = static::arrayPop($notes, 'tag') ?: $tag;
-            $new  = static::arrayPop($notes, '') ?: [];
-            $res .= static::renderTag($tag);
-            $save = $res;
-            foreach ($new as $hash => $lines) {
-                $res .= static::renderLines($lines);
-            }
-            foreach ($notes as $note => $cs) {
-                $note = static::arrayPop($cs, 'note');
-                $res .= static::renderNote($note);
-                foreach ($cs as $hash => $lines) {
-                    $res .= static::renderLines($lines);
-                }
-            }
-            if ($save === $res && stripos($tag, static::getVcs()->lastTag) !== false) {
-                $res = $prev;
-                unset($this->_tags[$tag]);
-            }
-        }
-
-        return $res;
-    }
-
-    public static function arrayPop(&$array, $key)
-    {
-        $res = $array[$key];
-        unset($array[$key]);
-
-        return $res;
-    }
-
-    public static function renderNote($note)
-    {
-        return "- $note\n";
-    }
-
-    public static function renderLines(array $lines)
-    {
-        $res = implode("\n", array_filter($lines));
-
-        return $res ? $res . "\n" : '';
-    }
-
-    public static function renderCommit($commit)
-    {
-        return static::skipCommit($commit) ? '' : "    - $commit[hash] $commit[date] $commit[comment] ($commit[email])";
-    }
-
     public static function skipCommit($commit)
     {
         $comment = $commit['comment'];
@@ -225,27 +171,4 @@ class History
         return false;
     }
 
-    public static function renderTag($tag)
-    {
-        if (strpos($tag, ' ') === false || $tag === static::getVcs()->initTag) {
-            $tag .= static::renderDate(static::getVcs()->tags[$tag]);
-        }
-
-        return "\n## $tag\n\n";
-    }
-
-    public static function renderDate($date)
-    {
-        return $date ? date(' Y-m-d', strtotime($date)) : '';
-    }
-
-    public static function renderHeader($string)
-    {
-        $res = '';
-        foreach ($this->_headers as $str) {
-            $res .= $str . "\n";
-        }
-
-        return $res;
-    }
 }
