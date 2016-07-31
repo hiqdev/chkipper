6c0f4b91c4df31dcf822f072e6bd6a8d30992ffa
Andrii Vasyliev <sol@hiqdev.com>
2016-07-31 17:53:27 +0300

    added NotesRenderer

---
+++ src/changelog/NotesRenderer.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Changelog keeper
+ *
+ * @link      https://github.com/hiqdev/chkipper
+ * @package   chkipper
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\chkipper\changelog;
+
+use hiqdev\chkipper\history\History;
+/**
+ * Release Notes renderer.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class NotesRenderer extends MarkdownRenderer
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function render(History $history)
+    {
+        $this->setHistory($history);
+        return $this->renderObjects('renderNote', $history->getFirstTag()->getNotes());
+    }
+
+}
