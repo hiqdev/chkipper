b28524359a2649985a613754e9c52d16a06ede1f
Andrii Vasyliev <sol@hiqdev.com>
2017-01-07 19:46:30 +0000

    added empty OrderLinks modifier

---
+++ src/lib/modifiers/OrderLinks.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Changelog keeper
+ *
+ * @link      https://github.com/hiqdev/chkipper
+ * @package   chkipper
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\chkipper\lib\modifiers;
+
+use hiqdev\chkipper\lib\History;
+
+/**
+ * Modifier that sorts links in configured order.
+ * Default order is:
+ * - other (user defined) links
+ * - author links
+ * - tags links
+ * - commit links
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class OrderLinks extends AbstractModifier
+{
+    protected $defaultOrder = ['other', 'authors', 'tags', 'commits'];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run(History $history)
+    {
+    }
+}
